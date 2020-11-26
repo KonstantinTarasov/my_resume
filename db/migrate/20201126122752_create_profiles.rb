aa176d5135bb5233836fd49ea58c60a705904175
SSmitt <lalaproger@yandex.ru>
2020-11-26 18:05:58 +0500

    The profile model was created

---
+++ db/migrate/20201126122752_create_profiles.rb
@@ -0,0 +1,14 @@
+class CreateProfiles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :profiles do |t|
+      t.string :name
+      t.string :surname
+      t.string :experience
+      t.integer :salary
+      t.string :email
+      t.string :telegram
+
+      t.timestamps
+    end
+  end
+end
