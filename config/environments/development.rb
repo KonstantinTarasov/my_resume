42680579ad4d3f2919f6d40eec6c887665fb9dfe
SSmitt <lalaproger@yandex.ru>
2020-12-03 19:39:58 +0500

    Сhanged settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,9 +67,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
-      :user_name            => 'kn.tv34',
-      :password             => '123456@kt',
-      :authentication       => "plain",
+      :user_name            => '',
+      :password             => '',
+      :authentication       => "",
       :enable_starttls_auto => true
   }
 end
